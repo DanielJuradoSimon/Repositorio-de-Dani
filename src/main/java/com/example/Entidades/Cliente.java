41769c0c5d27cad16f5f1189852859c08d158e3e
Daniel Jurado Simón <danieljuradosimon91@gmail.com>
2022-02-04 10:12:17 +0100

    commit 1

---
+++ src/main/java/com/example/Entidades/Cliente.java
@@ -0,0 +1,141 @@
+package com.example.Entidades;
+
+import java.util.List;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+
+
+/*NIF, apellidos, nombre, año de nacimiento, dirección, e-mail y teléfono de contacto*/
+
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "no puede estar vacío")
+	@Size(min = 9, max = 9, message = "el tamaño tiene que ser de 9 caracteres")
+	@Column(nullable = false)
+	private String DNI;
+	
+	@NotNull(message = "no puede estar vacío")
+	@Size(min = 4, max = 12, message = "el tamaño tiene que estar entre 4 y 12")
+	@Column(nullable = false)
+	private String nombre;
+	
+	@NotNull(message = "no puede estar vacío")
+	@Size(min = 4, max = 30, message = "el tamaño tiene que estar entre 4 y 12")
+	@Column(nullable = false)
+	private String apellidos;
+	
+	@NotNull(message = "no puede estar vacío")
+	@Column(name = "Fecha_de_nacimiento")
+	@Temporal(TemporalType.DATE)
+	private Date fecha_nac;
+	
+	@NotNull(message = "no puede estar vacío")
+	private String direccion;
+	
+	@NotNull(message = "no puede estar vacío")
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@NotNull(message = "no puede estar vacío")
+	@Size(min = 4, max = 12, message = "el tamaño tiene que estar entre 4 y 12")
+	@Column(nullable = false, unique = true, name = "teléfono")
+	private String telefono;
+	
+	@JsonIgnoreProperties(value={"cliente", "hibernateLazyInitializer", "handler"}, allowSetters=true)
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "cliente", cascade = CascadeType.ALL)
+	private List<Cuenta> cuentas;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public Date getFecha_nac() {
+		return fecha_nac;
+	}
+
+	public void setFecha_nac(Date fecha_nac) {
+		this.fecha_nac = fecha_nac;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public List<Cuenta> getCuentas() {
+		return cuentas;
+	}
+
+	public void setCuentas(List<Cuenta> cuentas) {
+		this.cuentas = cuentas;
+	}
+	
+	private static final long serialVersionUID = 1L;
+	
+}
---
+++ src/main/java/com/example/Entidades/Cuenta.java
@@ -0,0 +1,111 @@
+package com.example.Entidades;
+
+/*número de cuenta, fecha de creación y saldo que contiene.*/
+
+import java.util.List;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "cuentas")
+public class Cuenta {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull(message = "no puede estar vacío")
+	@Column(nullable = false, name="Numero_de_cuenta")
+	private String numCuenta; 
+	
+	@NotNull(message = "no puede estar vacío")
+	@Column(name = "Fecha_de_creacion")
+	@Temporal(TemporalType.DATE)
+	private Date fecha_nac;
+	
+	@Value("0")
+	@NotNull(message = "no puede estar vacío")
+	@Column(nullable = false)
+	private long saldo;
+	
+	@JsonIgnoreProperties(value={"cuenta", "hibernateLazyInitializer", "handler"}, allowSetters=true)
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "cuenta", cascade = CascadeType.ALL)
+	private List<Cliente> clientes;
+	
+	@JsonIgnoreProperties(value={"cuenta", "hibernateLazyInitializer", "handler"}, allowSetters=true)
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "cuenta", cascade = CascadeType.ALL)
+	private List<Operacion> Operaciones;
+
+	public List<Operacion> getOperaciones() {
+		return Operaciones;
+	}
+
+	public void setOperaciones(List<Operacion> operaciones) {
+		Operaciones = operaciones;
+	}
+	
+	public List<Cliente> getClientes() {
+		return clientes;
+	}
+
+	public void setClientes(List<Cliente> clientes) {
+		this.clientes = clientes;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNumCuenta() {
+		return numCuenta;
+	}
+
+	public void setNumCuenta(String numCuenta) {
+		this.numCuenta = numCuenta;
+	}
+
+	public Date getFecha_nac() {
+		return fecha_nac;
+	}
+
+	public void setFecha_nac(Date fecha_nac) {
+		this.fecha_nac = fecha_nac;
+	}
+
+	public long getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(long saldo) {
+		this.saldo = saldo;
+	}
+	
+	private static final long serialVersionUID = 1L;
+
+}
